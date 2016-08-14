492d4386da7ac58ff680bf8f1667675278e3ca7d
JOSS <JOSS@Josue>
2016-08-14 18:22:49 -0500

    Version 2

--- src/dto/alumnoDTO.java
+++ src/dto/alumnoDTO.java
@@ -10,5 +10,62 @@ package dto;
  * @author JOSS
  */
 public class alumnoDTO {
+    private int codigo;
+    private String nombre;
+    private String apellido;
+    private String curso;
+    private String profesor;
+
+    public alumnoDTO() {
+    }
+
+    public alumnoDTO(int codigo, String nombre, String apellido, String curso, String profesor) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.curso = curso;
+        this.profesor = profesor;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getProfesor() {
+        return profesor;
+    }
+
+    public void setProfesor(String profesor) {
+        this.profesor = profesor;
+    }
+    
     
 }
