e9fcc0427aaf5f881e0f9ebec5fffc4134f5d593
JOSS <JOSS@Josue>
2016-08-14 18:04:10 -0500

    Version 2

--- src/util/Conexion.java
+++ src/util/Conexion.java
@@ -5,10 +5,36 @@
  */
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author JOSS
  */
 public class Conexion {
+    private static final String URL = "jdbc:mysql://localhost:3306/asistencialp1";
+    private static final String DRIVER ="com.mysql.jdbc.Driver";
+    private static final String USER = "root";
+    private static final String PASS = "root";
+    private static Connection cx = null;
     
+    public static Connection getConexion(){
+        try {
+            Class.forName(DRIVER);
+            if(cx == null){
+            cx = DriverManager.getConnection(URL, USER, PASS);
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showMessageDialog(null, "Conexion: "+e);
+        }
+        return cx;
+    }
+    public static void cerrar(){
+        if(cx!=null){
+            cx = null;
+        }
+    }
 }
